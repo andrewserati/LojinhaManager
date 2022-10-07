125c5fd773fbd7ea4dbfa29d981aba4372158d24
Andrew Serati <andrewse2015@hotmail.com>
2022-10-06 21:52:14 -0300

    remove cached files

---
+++ LojinhaManager/LojinhaManagerAPI/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using LojinhaManagerAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojinhaManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public CustomerController() { }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return new List<Customer>()
+            {
+                new Customer(5, "Andrew", new DateTime(2000, 07, 05).ToUniversalTime())
+            };
+        }
+
+    }
+}
---
+++ LojinhaManager/LojinhaManagerAPI/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace LojinhaManagerAPI.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public Customer(int id, string name, DateTime birthdate) 
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Birthdate = birthdate;
+        }
+    }
+}
